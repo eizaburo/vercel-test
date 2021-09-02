86ba6a85217998de40fbf358da3c4c0e1c23da25
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2021-09-03 07:04:54 +0900

    カテゴリー追加

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -10,6 +10,9 @@ export default function BlogId({ blog }) {
                     __html: `${blog.body}`,
                 }}
             />
+            <div>
+                カテゴリー:{blog.category && `${blog.category.name}`}
+            </div>
         </main>
     );
 }
